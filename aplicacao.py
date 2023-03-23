a03792901cbf307918b9240c56d2f62fe21e0603
Sisnando <you@example.com>
2023-03-22 22:48:43 -0300

    Corrigido erro de conexão com API

--- aplicacao.py
+++ aplicacao.py
@@ -1,12 +1,30 @@
 # Construção da API --> Flask
 from flask import Flask, request
+import joblib
 
 # Instanciar o app
 app = Flask(__name__)
 
-@app.route('/api_preditivo')
-def funcao_01():
-    return {'Retorno' : 'Deu certo'}
+#Carregar nosso modelo
+Modelo = joblib.load('Modelo_Floresta_v100.pk1')
+
+#Função para receber nossa API
+@app.route('/api_preditivo/<area>;<rooms>;<bathroom>;<parking_spaces>;<floor>;<animal>;<furniture>;<hoa>;<property_tax>', methods = ['GET'])
+def funcao_01(area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax):
+    
+    # Recebendo os inputs da API
+    Lista = [
+        float(area), float(rooms), float(bathroom), float(parking_spaces), float(floor), float(animal), float(furniture), float(hoa), float(property_tax)
+    ]
+    
+    #Tentar a previsão
+    try:
+        Previsao = Modelo.predict([Lista])
+        return {"Valor_Aluguel" : str(Previsao)}
+    
+    except:
+        return {"Aviso" : "Deu algum erro!"}
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
