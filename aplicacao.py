4dc2910f2b8730169e290df2e52bb76ed9fe2bfd
Sisnando <you@example.com>
2023-03-22 23:31:06 -0300

    Projeto Concluído

--- aplicacao.py
+++ aplicacao.py
@@ -1,6 +1,8 @@
 # Construção da API --> Flask
 from flask import Flask, request
+from datetime import datetime
 import joblib
+import sqlite3
 
 # Instanciar o app
 app = Flask(__name__)
@@ -8,9 +10,13 @@ app = Flask(__name__)
 #Carregar nosso modelo
 Modelo = joblib.load('Modelo_Floresta_v100.pk1')
 
+
 #Função para receber nossa API
 @app.route('/api_preditivo/<area>;<rooms>;<bathroom>;<parking_spaces>;<floor>;<animal>;<furniture>;<hoa>;<property_tax>', methods = ['GET'])
 def funcao_01(area, rooms, bathroom, parking_spaces, floor, animal, furniture, hoa, property_tax):
+    
+    # Data e Hora de Inicio
+    Data_Inicio = datetime.now()
     
     # Recebendo os inputs da API
     Lista = [
@@ -19,7 +25,35 @@ def funcao_01(area, rooms, bathroom, parking_spaces, floor, animal, furniture, h
     
     #Tentar a previsão
     try:
+        # Predict
         Previsao = Modelo.predict([Lista])
+        
+        # Inserir o valor da Previsão
+        Lista.append(str(Previsao))
+        
+        # Transformando Lista em String
+        Input = ''
+        for Valor in Lista:
+            Input = Input + ';' + str(Valor)
+        
+        # Capturando data e hora
+        Data_Fim = datetime.now()
+        Processamento = Data_Fim - Data_Inicio
+        
+        # Conexão com o banco de dados
+        Conexao_Banco = sqlite3.connect('banco_api.db')
+        Cursor = Conexao_Banco.cursor()
+        
+        # Query
+        Query_Inserindo_Dados = f'''
+        INSERT INTO Log_API (Inputs, Inicio, Fim, Processamento) 
+        VALUES ( '{Input}', '{Data_Inicio}', '{Data_Fim}', '{Processamento}')
+        '''
+        # Executar a query
+        Cursor.execute(Query_Inserindo_Dados)
+        Conexao_Banco.commit()
+        Cursor.close()
+        
         return {"Valor_Aluguel" : str(Previsao)}
     
     except:
